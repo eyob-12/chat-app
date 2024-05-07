ba9e6e5374f722813ab63e20ab911adb37f6bee2
turbo2121 <eyobaddis394@gmail.com>
2024-05-07 09:36:24 +0300

    initial commit

--- server/config/connectDB.js
+++ server/config/connectDB.js
@@ -1,1 +1,13 @@
 
+const mongoose = require("mongoose");
+async function connectDb() {
+    try {
+        const connect = await mongoose.connect(process.env.MONGO_CONNECTION_STRING);
+        console.log("DATABASE CONNECTED ....");
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+};
+
+module.exports = connectDb;
--- server/index.js
+++ server/index.js
@@ -13,11 +13,13 @@ const fs = require('fs');
 dotenv.config();
 mongoose.connect(process.env.MONGO_URL, (err) => {
   if (err) throw err;
+  console.log("DATABASE CONNECTED ....");
 });
 const jwtSecret = process.env.JWT_SECRET;
 const bcryptSalt = bcrypt.genSaltSync(10);
 
 const app = express();
+
 app.use('/uploads', express.static(__dirname + '/uploads'));
 app.use(express.json());
 app.use(cookieParser());
@@ -41,27 +43,27 @@ async function getUserDataFromRequest(req) {
 
 }
 
-app.get('/test', (req,res) => {
+app.get('/test', (req, res) => {
   res.json('test ok');
 });
 
-app.get('/messages/:userId', async (req,res) => {
-  const {userId} = req.params;
+app.get('/messages/:userId', async (req, res) => {
+  const { userId } = req.params;
   const userData = await getUserDataFromRequest(req);
   const ourUserId = userData.userId;
   const messages = await Message.find({
-    sender:{$in:[userId,ourUserId]},
-    recipient:{$in:[userId,ourUserId]},
-  }).sort({createdAt: 1});
+    sender: { $in: [userId, ourUserId] },
+    recipient: { $in: [userId, ourUserId] },
+  }).sort({ createdAt: 1 });
   res.json(messages);
 });
 
-app.get('/people', async (req,res) => {
-  const users = await User.find({}, {'_id':1,username:1});
+app.get('/people', async (req, res) => {
+  const users = await User.find({}, { '_id': 1, username: 1 });
   res.json(users);
 });
 
-app.get('/profile', (req,res) => {
+app.get('/profile', (req, res) => {
   const token = req.cookies?.token;
   if (token) {
     jwt.verify(token, jwtSecret, {}, (err, userData) => {
@@ -73,14 +75,14 @@ app.get('/profile', (req,res) => {
   }
 });
 
-app.post('/login', async (req,res) => {
-  const {username, password} = req.body;
-  const foundUser = await User.findOne({username});
+app.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+  const foundUser = await User.findOne({ username });
   if (foundUser) {
     const passOk = bcrypt.compareSync(password, foundUser.password);
     if (passOk) {
-      jwt.sign({userId:foundUser._id,username}, jwtSecret, {}, (err, token) => {
-        res.cookie('token', token, {sameSite:'none', secure:true}).json({
+      jwt.sign({ userId: foundUser._id, username }, jwtSecret, {}, (err, token) => {
+        res.cookie('token', token, { sameSite: 'none', secure: true }).json({
           id: foundUser._id,
         });
       });
@@ -88,25 +90,25 @@ app.post('/login', async (req,res) => {
   }
 });
 
-app.post('/logout', (req,res) => {
-  res.cookie('token', '', {sameSite:'none', secure:true}).json('ok');
+app.post('/logout', (req, res) => {
+  res.cookie('token', '', { sameSite: 'none', secure: true }).json('ok');
 });
 
-app.post('/register', async (req,res) => {
-  const {username,password} = req.body;
+app.post('/register', async (req, res) => {
+  const { username, password } = req.body;
   try {
     const hashedPassword = bcrypt.hashSync(password, bcryptSalt);
     const createdUser = await User.create({
-      username:username,
-      password:hashedPassword,
+      username: username,
+      password: hashedPassword,
     });
-    jwt.sign({userId:createdUser._id,username}, jwtSecret, {}, (err, token) => {
+    jwt.sign({ userId: createdUser._id, username }, jwtSecret, {}, (err, token) => {
       if (err) throw err;
-      res.cookie('token', token, {sameSite:'none', secure:true}).status(201).json({
+      res.cookie('token', token, { sameSite: 'none', secure: true }).status(201).json({
         id: createdUser._id,
       });
     });
-  } catch(err) {
+  } catch (err) {
     if (err) throw err;
     res.status(500).json('error');
   }
@@ -114,13 +116,13 @@ app.post('/register', async (req,res) => {
 
 const server = app.listen(4040);
 
-const wss = new ws.WebSocketServer({server});
+const wss = new ws.WebSocketServer({ server });
 wss.on('connection', (connection, req) => {
 
   function notifyAboutOnlinePeople() {
     [...wss.clients].forEach(client => {
       client.send(JSON.stringify({
-        online: [...wss.clients].map(c => ({userId:c.userId,username:c.username})),
+        online: [...wss.clients].map(c => ({ userId: c.userId, username: c.username })),
       }));
     });
   }
@@ -151,7 +153,7 @@ wss.on('connection', (connection, req) => {
       if (token) {
         jwt.verify(token, jwtSecret, {}, (err, userData) => {
           if (err) throw err;
-          const {userId, username} = userData;
+          const { userId, username } = userData;
           connection.userId = userId;
           connection.username = username;
         });
@@ -161,22 +163,22 @@ wss.on('connection', (connection, req) => {
 
   connection.on('message', async (message) => {
     const messageData = JSON.parse(message.toString());
-    const {recipient, text, file} = messageData;
+    const { recipient, text, file } = messageData;
     let filename = null;
     if (file) {
       console.log('size', file.data.length);
       const parts = file.name.split('.');
       const ext = parts[parts.length - 1];
-      filename = Date.now() + '.'+ext;
+      filename = Date.now() + '.' + ext;
       const path = __dirname + '/uploads/' + filename;
       const bufferData = new Buffer(file.data.split(',')[1], 'base64');
       fs.writeFile(path, bufferData, () => {
-        console.log('file saved:'+path);
+        console.log('file saved:' + path);
       });
     }
     if (recipient && (text || file)) {
       const messageDoc = await Message.create({
-        sender:connection.userId,
+        sender: connection.userId,
         recipient,
         text,
         file: file ? filename : null,
@@ -186,10 +188,10 @@ wss.on('connection', (connection, req) => {
         .filter(c => c.userId === recipient)
         .forEach(c => c.send(JSON.stringify({
           text,
-          sender:connection.userId,
+          sender: connection.userId,
           recipient,
           file: file ? filename : null,
-          _id:messageDoc._id,
+          _id: messageDoc._id,
         })));
     }
   });
