b6e4f8eaacee522ee0f1370ca598a361b490f4e3
turbo2121 <eyobaddis394@gmail.com>
2024-05-07 13:28:55 +0300

    Updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,9 @@
 import axios from "axios";
-import {UserContextProvider} from "./UserContext";
+import { UserContextProvider } from "./UserContext";
 import Routes from "./Routes";
 
 function App() {
-  axios.defaults.baseURL = 'http://localhost:4040';
+  axios.defaults.baseURL = 'https://chat-app-d9n4.onrender.com';
   axios.defaults.withCredentials = true;
   return (
     <UserContextProvider>
